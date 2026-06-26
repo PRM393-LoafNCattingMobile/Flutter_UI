3b09d3f4bf77fec2258643455b484d6ad4d7eba2
Acharné <maiquocanh2608@gmail.com>
2026-06-26 12:08:35 +0700

    chore: localize product recommendation headings

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -142,8 +142,8 @@ class AppStrings {
   static const cartSyncFailedMessage =
       'Chưa thể cập nhật giỏ hàng. Vui lòng thử lại.';
   static const addToCartButton = 'Thêm vào giỏ';
-  static const perfectWithTitle = 'Perfect with';
-  static const similarItemsTitle = 'Similar items';
+  static const perfectWithTitle = 'Hợp với';
+  static const similarItemsTitle = 'Món tương tự';
   static const retryButton = 'Thử lại';
   static const unknownBreed = 'Chưa rõ giống';
 
